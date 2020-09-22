915ecc15a4b6bfe831902f601ae532b781c3ae50
michael <nguyen.hoang.michael@gmail.com>
2020-09-22 08:45:15 +0200

    Fixes

--- src/App.js
+++ src/App.js
@@ -70,12 +70,14 @@ class App extends React.Component {
     }
 
     // Usage: <input type="text" value={this.state.username} onChange={this.handleChangeInput('username')} /> 
+    /*
     handleChangeInput = (name) => (event) => {
         let nouvelleValeur = event.target.value;
         this.setState({
             [name]: nouvelleValeur
         });
     }
+    */
 
     render() {
 
